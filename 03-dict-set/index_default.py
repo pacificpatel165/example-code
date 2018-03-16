45d105e2fc79829324eef11de7c36e07f2d2f56e
Prashant Patel <itprashantpatel@gmail.com>
2018-03-16 17:10:36 +0530

    Added zen.txt file for chapter 3

--- 03-dict-set/index_default.py
+++ 03-dict-set/index_default.py
@@ -12,7 +12,7 @@ import collections
 WORD_RE = re.compile('\w+')
 
 index = collections.defaultdict(list)     # <1>
-with open(sys.argv[1], encoding='utf-8') as fp:
+with open('zen.txt', encoding='utf-8') as fp:
     for line_no, line in enumerate(fp, 1):
         for match in WORD_RE.finditer(line):
             word = match.group()
